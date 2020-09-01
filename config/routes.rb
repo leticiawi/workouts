153e5376025daecbadf3bd274ea7455c066a0bb0
Giovanna Betti <giovannabetti@gmail.com>
2020-08-31 21:08:04 -0300

    Inserting some authentications.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'user_dashboards/index'
   devise_for :users
+
+  resources :profiles, only: [:new, :create]
+  resources :user_dashboards, only: [:index]
+  resources :trainnings
+
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :trainnings
 end
