2232176cbfef997c0625257a126ea8c61e43373e
pedrotrias <pedropedro0225@gmail.com>
2020-09-04 12:32:08 -0300

    home page fixed and the profile edited

--- app/controllers/trainnings_controller.rb
+++ app/controllers/trainnings_controller.rb
@@ -2,9 +2,11 @@ class TrainningsController < ApplicationController
   def index
     @trainnings = Trainning.all
   end
+
   def show
     @trainning = Trainning.find(params[:id])
   end
+
   def new
     if current_user.trainer == true
     @trainning = Trainning.new
@@ -13,19 +15,23 @@ class TrainningsController < ApplicationController
     redirect_to root_path
   end
 end
+
   def create
     @trainning = Trainning.new(trainnings_params)
     @trainning.user = current_user
     @trainning.category = Category.find(params[:trainning][:category])
+
     if @trainning.save
       redirect_to trainer_show_path(@trainning)
     else
       render :new
     end
   end
+
   def edit
     @trainning = Trainning.find(params[:id])
   end
+
   def update
     @trainning = Trainning.find(params[:id])
     @trainning.category = Category.find(params[:trainning][:category])
@@ -36,18 +42,23 @@ end
       render :edit
     end
   end
+
   def destroy
     @trainning = Trainning.find(params[:id])
     @trainning.destroy
     redirect_to trainer_index_path
   end
+
   def trainer_index
     @trainnings = Trainning.where(user: current_user)
   end
+
   def trainer_show
     @trainning = Trainning.find(params[:id])
   end
+
   private
+
   def trainnings_params
     params.require(:trainning).permit(:duration, :address, :description, :photo)
   end
