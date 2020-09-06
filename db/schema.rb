78013c1d5f0d59bfd9e6e0b2486b3183d2ce48d8
leticia wiermann <leticia.wiermann@gmail.com>
2020-09-06 19:21:12 -0300

    coloquei os cards de detalhes do trerno do treinador dentro do show de treinador, assim ele pode deletar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_04_165539) do
+ActiveRecord::Schema.define(version: 2020_09_06_214904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,11 +43,11 @@ ActiveRecord::Schema.define(version: 2020_09_04_165539) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.text "review"
     t.bigint "user_id", null: false
     t.bigint "trainning_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "Booked_for"
     t.index ["trainning_id"], name: "index_orders_on_trainning_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2020_09_04_165539) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "latitude"
     t.float "longitude"
+    t.integer "price_cents", default: 0, null: false
     t.index ["category_id"], name: "index_trainnings_on_category_id"
     t.index ["user_id"], name: "index_trainnings_on_user_id"
   end
