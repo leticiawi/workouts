fffd8ee35bd0e0521908b2c56f4fb1b13d2066b9
Giovanna Betti <giovannabetti@gmail.com>
2020-09-08 12:14:41 -0300

    Content class finished. Starting whatch a kitt tutorial

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,5 +1,7 @@
 class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
+    # acima, sigo a aula. Abaixo sigo o tutorial do kitt.
+    # @chatroom = Chatroom.includes(messages: :user).find(params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
   has_one :profile, dependent: :destroy
   # phone
 
+  has_many :messages, dependent: :destroy
   # geocoder info
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
