21a2137cef2948d4e23d71d92f7b441e7aa22ea7
Giovanna Betti <giovannabetti@gmail.com>
2020-09-08 18:45:08 -0300

    Comecando chat do tutorial

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,8 +1,5 @@
 class ChatroomsController < ApplicationController
   def show
-    @chatroom = Chatroom.find(params[:id])
-    @message = Message.new
-    # acima, sigo a aula. Abaixo sigo o tutorial do kitt.
     # @chatroom = Chatroom.includes(messages: :user).find(params[:id])
   end
 end
