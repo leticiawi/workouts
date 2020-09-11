df7874fc8f773f424df2414f84f80c66782fb630
leticia wiermann <leticia.wiermann@gmail.com>
2020-09-11 11:12:09 -0300

    all things fixed

--- app/controllers/trainnings_controller.rb
+++ app/controllers/trainnings_controller.rb
@@ -3,14 +3,15 @@ class TrainningsController < ApplicationController
 
   def index
     @profile = current_user.profile
-    @trainnings = Trainning.geocoded.where(category_id: params[:category_id], active: true)
+
     @category_id = params[:category_id]
     if params[:category_id]
-      User.where("user != current_use AND category_id: @category_id ")
-      @trainnings
-    elsif params[:search]
-      @trainnings = @trainnings.near(params[:search][:address], 15)
+      @trainnings = Trainning.geocoded.where(category_id: params[:category_id], active: true)
 
+    elsif params[:search]
+      @trainnings = Trainning.geocoded.where(active: true).near(params[:search][:address], 15)
+    else
+      @trainnings = Trainning.geocoded.where(active: true)
     end
     @markers = @trainnings.geocoded.map do |trainning|
       {
